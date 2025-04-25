3af0a391020e2adf5fdef599be6733b12615c13f
SilverRowan <silverrowan@gmail.com>
2025-04-25 16:03:08 -0700

    Create boilerplate and plan for sketch app
        new file:   index.html
        new file:   script.js
    	new file:   style.css

---
+++ script.js
@@ -0,0 +1,17 @@
+// ==============Create Grid==============
+// get size of grid: on load 16x16, later by user input
+    // let gridSize = 16 //initially, to be updated by user input
+    // iterate gridSize times
+        // create one div gridSize, class "row",
+        // iterate gridsize times
+            // create div inside row class div, class "column",
+
+// ==============Set Up Mouse Trace==============
+// "hover" effect - make grid divs change color when mouse passes over them, and remain after it leaves
+    //add "trace" class to div
+
+// ==============Create User Interface==============
+//button @ top of screen that opens a popup asking for new grid size
+    //once entered prev grid to be REMOVED
+    //new grid generated in the SAME TOTAL SPACE as before
+    //set maximum to 100 to avoid freezing etc.
