6ee13c6099ad43a1d6276a91301e24a58288b6ef
SilverRowan <silverrowan@gmail.com>
2025-11-15 01:34:42 -0800

    Display proper layout (16x16) w outlines

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # etchASketch
 Etch-a-sketch like project per The Odin Project 
+
+
+Instructions
+----------------
+create webpage
+in HTML: create div container for....
+in JS: create grid of <div>s (16x16)
+in CSS: use flexbox to make the divs appear as a grid
+
+Set up a 'hover' effect - change grid div when mouse passes over them, leaving a trail.
+Add a button at the top of the screen : send the user a pop-up asking for number of squares for a new grid
+    once entered, the exisiting grid should be REMOVED, and a NEW grid generated in the *same space* (same # pixels).
+    (set limit on user input to 100 max)
+
+-----------------
+bonus options:
+    a. randomize the square's rgb color w ea interaction
+    b. progressive darkening where each interaction w a square by 10% (fully black in 10 interactions)
--- scriptEtchASketch.js
+++ scriptEtchASketch.js
@@ -1,1 +1,18 @@
-console.log('test');
+const headderBarDiv = document.querySelector('div.headderBar');
+    headderBarDiv.className = 'btnFrame'
+    const newGridBtn = document.createElement('button');
+    newGridBtn.className = `newGrid`
+    newGridBtn.textContent = 'New Grid';
+    headderBarDiv.appendChild(newGridBtn);
+const gridContainerDiv = document.querySelector('div.gridContainer');
+    gridContainerDiv.className = 'frame';
+    for (i=0; i < 16; i++) {
+        const rowContainderDiv = document.createElement('div');
+        rowContainderDiv.className = `column co${i}`;
+        for (j=0; j < 16; j++) {
+            const gridCellDiv = document.createElement('div')
+            gridCellDiv.className = `gridCell gc${j}`;
+            rowContainderDiv.appendChild(gridCellDiv);
+        }
+        gridContainerDiv.appendChild(rowContainderDiv);
+    }
